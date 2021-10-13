8e68ae62465f45f00d505abcd2c8d1b0e4bd3211
jschenkelberg <jmschenkelberg@gmail.com>
2021-10-13 15:29:33 -0500

    added get request and sample table

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Navigation from './Navbar/navbar';
+import axios from 'axios';
+import ApplicationTable from './ApplicationTable/applicationTable';
 
 function App() {
+  const [applications, setApplications] = useState([ ]);
+
+  const getAllApplications = async () => {
+    let res = await axios(`https://localhost:44394/api/applications`);
+    if(res.data.length !== 0){
+      setApplications(res.data)
+      console.log(res.data)
+    }
+  };
+
+  useEffect(() => {
+  getAllApplications();
+  }, []);
+
   return (
     <React.Fragment>
       <Navigation />
+      <ApplicationTable />
     </React.Fragment>
   );
 }
